379de3fa5c5f277f9a049ad6ac746a8da858380f
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-14 11:27:20 +1000

    Initial import

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+swift build -Xlinker -L/usr/local/lib -Xcc -I/usr/local/include/glib-2.0/ -Xcc -I/usr/local/include
