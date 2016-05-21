fd9fceff51ff700997d6517625a346d74352540f
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-22 07:38:00 +1000

    Add freestanding #includes

--- glib_bridging.h
+++ glib_bridging.h
@@ -1,42 +1,65 @@
-struct GAsyncQueue {};
-struct GBookmarkFile {};
-struct GBytes {};
-struct GChecksum {};
-struct GData {};
-struct GDateTime {};
-struct GDir {};
-struct GHashTable {};
-struct GHmac {};
-struct GKeyFile {};
-struct GMainContext {};
-struct GMainLoop {};
-struct GMappedFile {};
-struct GMarkupParseContext {};
-struct GMatchInfo {};
-struct GMutexLocker {};
-struct GOptionContext {};
-struct GOptionGroup {};
-struct GPatternSpec {};
-struct GRand {};
-struct GRegex {};
-struct GSequence {};
-struct GSequenceIter {};
-struct GSourcePrivate {};
-struct GStatBuf {};
-struct GStringChunk {};
-struct GString_autoptr {};
-struct GTestCase {};
-struct GTestSuite {};
-struct GTimeZone {};
-struct GTimer {};
-struct GTree {};
-struct GVariant {};
+struct _GAsyncQueue {};
+struct _GBookmarkFile {};
+struct _GBytes {};
+struct _GChecksum {};
+struct _GData {};
+struct _GDateTime {};
+struct _GDir {};
+struct _GHashTable {};
+struct _GHmac {};
+struct _GKeyFile {};
+struct _GMainContext {};
+struct _GMainLoop {};
+struct _GMappedFile {};
+struct _GMarkupParseContext {};
+struct _GMatchInfo {};
+struct _GMutexLocker {};
+struct _GOptionContext {};
+struct _GOptionGroup {};
+struct _GPatternSpec {};
+struct _GRand {};
+struct _GRegex {};
+struct _GSequence {};
+struct _GSequenceIter {};
+struct _GSourcePrivate {};
+struct _GStringChunk {};
+struct _GTestCase {};
+struct _GTestSuite {};
+struct _GTimeZone {};
+struct _GTimer {};
+struct _GTree {};
+struct _GVariant {};
 struct _GVariantType {};
-struct Mutex {};
+struct _GMutex {};
+struct _GRecMutex {};
 
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
 #include <glib-unix.h>
 #include <glib-object.h>
-#include <glib/gvarianttype.h>
-#include <glib/gthread.h>
 #include <glib/gstdio.h>
+#include <glib/garray.h>
+#include <glib/gasyncqueue.h>
+#include <glib/gbookmarkfile.h>
+#include <glib/gchecksum.h>
+#include <glib/gdataset.h>
+#include <glib/gdatetime.h>
+#include <glib/gdir.h>
+#include <glib/ghash.h>
+#include <glib/ghmac.h>
+#include <glib/gkeyfile.h>
+#include <glib/gmain.h>
+#include <glib/gmappedfile.h>
+#include <glib/gmarkup.h>
+#include <glib/gregex.h>
+#include <glib/gthread.h>
+#include <glib/goption.h>
+#include <glib/gpattern.h>
+#include <glib/grand.h>
+#include <glib/gsequence.h>
+#include <glib/gstringchunk.h>
+#include <glib/gtestutils.h>
+#include <glib/gtimezone.h>
+#include <glib/gtimer.h>
+#include <glib/gtree.h>
+#include <glib/gvariant.h>
+#include <glib/gvarianttype.h>
